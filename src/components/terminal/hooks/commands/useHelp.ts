89f612fe4e9a5abd55994df376847405a75e8e5c
Brett Holman <brettholman91@gmail.com>
2023-10-16 16:08:15 -0700

    Update help message a bit

--- src/components/terminal/hooks/commands/useHelp.ts
+++ src/components/terminal/hooks/commands/useHelp.ts
@@ -3,11 +3,12 @@ import { CommandStatusCode } from "../../models/commandStatusCode";
 
 const output = `There are various commands that are supported. Providing the argument "help" to any command will also provide additional context.
 Currently the list of supported commands are as follows:
-  - help (this current command, unfortunently \`help help\` won't give you anthing interesting)
+  - help (prints this message, no arguments accepted at this time)
   - about
   - kexp
   - pwd
-Give any of the commands a shot! You can start typing at any point (or navigate below to choose a different screen)`;
+  - clear
+Give any of the commands a shot! You can start typing at any point (or navigate below to choose a different tab)`;
 
 export const useHelp = () => {
   const help = (): CommandResponse => {
