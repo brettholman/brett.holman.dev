a1422fb43af2198af124ae862d948b61fc94c80a
ErikHolman <lifeinthedayof@gmail.com>
2023-10-17 11:15:24 -0700

    migrate to flexbox

--- src/components/terminal/components/footer.tsx
+++ src/components/terminal/components/footer.tsx
@@ -1,16 +1,19 @@
-import { makeStyles } from "@material-ui/styles";
-import { Grid } from "@mui/material";
-import { CenterStatusBar, LeftStatusBar, RightStatusBar } from "./statusBar";
-import { SessionState } from "../models/sessionState";
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@mui/material';
+import { CenterStatusBar, LeftStatusBar, RightStatusBar } from './statusBar';
+import { SessionState } from '../models/sessionState';
 
 const useStyles = makeStyles({
   footer: {
-    marginTop: "1rem",
-    padding: "1rem",
-    position: "fixed",
+    marginTop: '1rem',
+    padding: '1rem',
+    position: 'fixed',
     bottom: 0,
     left: 0,
-    width: "100%",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
@@ -26,26 +29,18 @@ export const Footer = ({ sessionState, updateSessionState }: FooterProps) => {
   const isMobile = width <= 768;
 
   return (
-    <Grid container className={classes.footer}>
-      <Grid item lg={4} sm={0}>
-        <LeftStatusBar
-          sessionName="portfolio"
-          tabPosition={1}
-          panePosition={0}
-          isMobile={isMobile}
-        />
-      </Grid>
-      <Grid item lg={4} sm={12}>
-        <Grid container justifyContent="center">
-          <CenterStatusBar
-            sessionState={sessionState}
-            updateSessionState={updateSessionState}
-          />
-        </Grid>
-      </Grid>
-      <Grid item lg={4} sm={0}>
-        <RightStatusBar isMobile={isMobile} />
-      </Grid>
-    </Grid>
+    <div className={classes.footer}>
+      <LeftStatusBar
+        sessionName='portfolio'
+        tabPosition={1}
+        panePosition={0}
+        isMobile={isMobile}
+      />
+      <CenterStatusBar
+        sessionState={sessionState}
+        updateSessionState={updateSessionState}
+      />
+      <RightStatusBar isMobile={isMobile} />
+    </div>
   );
 };
