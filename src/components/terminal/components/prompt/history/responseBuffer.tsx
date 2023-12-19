fe8799bdc8b55f1f9ebfcc3b9a7bf003f9021a75
Brett Holman <brettholman91@gmail.com>
2023-12-19 00:44:05 -0800

    Fix issue with opening repo in new tab

--- src/components/terminal/components/prompt/history/responseBuffer.tsx
+++ src/components/terminal/components/prompt/history/responseBuffer.tsx
@@ -8,6 +8,8 @@ interface ResponseBufferProps {
 // @see https://medium.com/@uigalaxy7/how-to-render-html-in-react-7f3c73f5cafc
 // Using an html sanatizer to help
 
+const internalSanitize = (value: string) => sanitize(value, { ADD_ATTR: ['target', 'rel'] });
+
 export const ResponseBuffer = ({ value }: ResponseBufferProps) => {
-  return <pre dangerouslySetInnerHTML={{ __html: sanitize(value) }} style={{ paddingLeft: "1em", whiteSpace: "pre-wrap" }}></pre>
+  return <pre dangerouslySetInnerHTML={{ __html: internalSanitize(value) }} style={{ paddingLeft: "1em", whiteSpace: "pre-wrap" }}></pre>
 };
