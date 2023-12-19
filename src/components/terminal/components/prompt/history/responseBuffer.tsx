929a65bab9a7a848f080611229af502062b8c1f6
Brett Holman <brettholman91@gmail.com>
2023-12-19 09:45:37 -0800

    Formatting in responseBuffer

--- src/components/terminal/components/prompt/history/responseBuffer.tsx
+++ src/components/terminal/components/prompt/history/responseBuffer.tsx
@@ -4,12 +4,8 @@ interface ResponseBufferProps {
   value: string;
 }
 
-// TODO there are better solutions to rendering the html from responses.
-// @see https://medium.com/@uigalaxy7/how-to-render-html-in-react-7f3c73f5cafc
-// Using an html sanatizer to help
-
-const internalSanitize = (value: string) => sanitize(value, { ADD_ATTR: ['target', 'rel'] });
-
-export const ResponseBuffer = ({ value }: ResponseBufferProps) => {
-  return <pre dangerouslySetInnerHTML={{ __html: internalSanitize(value) }} style={{ paddingLeft: "1em", whiteSpace: "pre-wrap" }}></pre>
-};
+export const ResponseBuffer = ({ value }: ResponseBufferProps) =>
+  <pre
+    dangerouslySetInnerHTML={{ __html: sanitize(value, { ADD_ATTR: ['target', 'rel'] }) }}
+    style={{ paddingLeft: "1em", whiteSpace: "pre-wrap" }}
+  />
