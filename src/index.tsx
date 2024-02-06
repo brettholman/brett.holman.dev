a1ea593da40b46e86b586d062021f2fff99f7829
Brett Holman <brett@holman.dev>
2024-02-06 00:08:48 -0800

    Fix input for theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import { Theme } from "./theme/theme";
-import { ThemeProvider } from "@emotion/react";
 import { Router } from "./router/router";
+import { ThemeProvider } from "@mui/material";
 
 // it could be your App.tsx file or theme file that is included in your tsconfig.json
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 const theme = Theme();
@@ -20,7 +20,7 @@ root.render(
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
